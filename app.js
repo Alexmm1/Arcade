78d19c36b92739b7a88be94665d9cf3d731abbae
Alexander <alexmayeux64@gmail.com>
2023-02-09 15:49:29 -0600

    draft 4

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let board = Array.from(document.getElementsByClassName('cell'))
-let restart = document.getElementsByClassName('game-restart')
+let restart = document.getElementsByTagName('button')
 let spaces = Array(9).fill(null)
 let x_test = "X"
 let o_test = "O"
 let currentPlayer = x_test
+let playerText = document.getElementById('playerText')
+let playername = document.getElementById('player-title')
+let playername2 = document.getElementById('player-title2')
 const winningCombo = [
     [0,1,2],
     [3,4,5],
@@ -14,23 +17,40 @@ const winningCombo = [
     [0,4,8],
     [2,4,6],
 ]
-console.log(spaces)
 const startGame = () => {
-    board.forEach(board => board.addEventListener('click', boardClick))
+    board.forEach(cell => cell.addEventListener('click', boardClick))
 }
 function boardClick(e) {
-    console.log(e.target)
-    // const id = e.target.id
-    // if(!spaces[id]){
-    //     spaces[id] = currentPlayer
-    //     e.target.innerText = currentPlayer
-    //     currentPlayer = currentPlayer == x_test ? o_test : x_test
-    // }
+    const id = e.target.id
+    if(!spaces[id]){
+        spaces[id] = currentPlayer
+        e.target.innerText = currentPlayer
+        currentPlayer = currentPlayer === x_test ? o_test : x_test
+
+        if(playerHasWon() !==false){
+            playerText = `$(currentPlayer) has won`
+        }
+    }
 }
-// function playerHasWon() {
-//     for (const condition of winningCombo) {
-//         let [a, b, c] = condition
-//     }
-// }
+
+//restart.addEventListener('click', restartBtn)
+function restartBtn(){
+    spaces.fill(null)
+
+    board.forEach( board => {
+        board.innerText = ''
+        currentPlayer = x_test
+    })
+}
+function playerHasWon() {
+    for (const condition of winningCombo) {
+        let [a, b, c] = condition  
+
+        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])){
+            return [a,b,c]
+        }
+    }
+    return false
+}
 
 startGame()
