1c04529ec8b6c51b3b76581fd0848ac8bc9a1e21
Alexander <alexmayeux64@gmail.com>
2023-02-12 15:00:05 -0600

    Draft

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 let board = Array.from(document.getElementsByClassName('cell'))
-let restart = document.getElementsByTagName('button')
+let restart = document.getElementById('restart')
 let spaces = Array(9).fill(null)
 let x_test = "X"
 let o_test = "O"
 let currentPlayer = x_test
 let playerText = document.getElementById('playerText')
-let playername = document.getElementById('player-title')
-let playername2 = document.getElementById('player-title2')
+let winnerText = document.getElementsByTagName('p')[0]
+let playerName = document.getElementById('player-title')
+let playerName2 = document.getElementById('player-title2')
 const winningCombo = [
     [0,1,2],
     [3,4,5],
@@ -17,6 +18,8 @@ const winningCombo = [
     [0,4,8],
     [2,4,6],
 ]
+const playerName1Btn = document.getElementById('playerName1Btn')
+const playerName2Btn = document.getElementById('playerName2Btn')
 const startGame = () => {
     board.forEach(cell => cell.addEventListener('click', boardClick))
 }
@@ -25,15 +28,20 @@ function boardClick(e) {
     if(!spaces[id]){
         spaces[id] = currentPlayer
         e.target.innerText = currentPlayer
-        currentPlayer = currentPlayer === x_test ? o_test : x_test
-
-        if(playerHasWon() !==false){
-            playerText = `$(currentPlayer) has won`
-        }
+       currentPlayer =o_test
+    } else{
+        return currentPlayer
     }
+
+    if(playerHasWon() !==false){
+        playerText = `${currentPlayer} has won!`
+        console.log(playerText)
+        alert(playerText)
+    }
+  
 }
 
-//restart.addEventListener('click', restartBtn)
+restart.addEventListener('click', restartBtn)
 function restartBtn(){
     spaces.fill(null)
 
@@ -49,8 +57,17 @@ function playerHasWon() {
         if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])){
             return [a,b,c]
         }
+
     }
     return false
 }
 
+
+playerName1Btn.addEventListener('click', function(evt){
+    evt.preventDefault ()
+})
+
+playerName2Btn.addEventListener('click', function(evt){
+    evt.preventDefault ()
+})
 startGame()
