2949cb11556d8bde957ee398391722e1b94224c4
Alexander <alexmayeux64@gmail.com>
2023-04-04 14:43:47 -0500

    Set the computer player up. Need to fix the player name not showing up error

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let board = Array.from(document.getElementsByClassName('cell'))
-let restart = document.getElementById('restart')
+let restartBtn = document.getElementById('restart')
 let spaces = Array(9).fill(null)
 let x_test = "X"
 let o_test = "O"
 let currentPlayer = x_test
-let playerText = document.getElementById('playerText')
+let player1Name = '';
+let player2Name = '';
+const playerName1Input = document.getElementById('playerName1Input')
+const playerName2Input = document.getElementById('playerName2Input')
+const playerName1Btn = document.getElementById('playerName1Btn')
+const playerName2Btn = document.getElementById('playerName2Btn')
 let playerName = document.getElementById('player-title')
 let playerName2 = document.getElementById('player-title2')
 const winningCombo = [
@@ -17,8 +22,7 @@ const winningCombo = [
     [0,4,8],
     [2,4,6],
 ]
-const playerName1Btn = document.getElementById('playerName1Btn')
-const playerName2Btn = document.getElementById('playerName2Btn')
+
 const startGame = () => {
     board.forEach(cell => cell.addEventListener('click', boardClick))
 }
@@ -29,46 +33,82 @@ function boardClick(e) {
     if(!spaces[id]){
         spaces[id] = currentPlayer
         e.target.innerText = currentPlayer
-       currentPlayer =o_test
+        const winner = playerHasWon();
+        if(winner !==false){
+            playerText = `${winner} has won!`
+            alert(playerText)
+            return
+        }
+        currentPlayer = currentPlayer == x_test ? o_test : x_test
+        if (currentPlayer == o_test) {
+            setTimeout(() => computerMove(), 1000)
+        }
+        updatePlayerName()
     } 
+
+}
+
+function computerMove() {
+    let id
+    do {
+        id = Math.floor(Math.random() * 9)
+    } while (spaces[id])
+    spaces[id] = currentPlayer
+    document.getElementById(id).innerText = currentPlayer
 
     if(playerHasWon() !==false){
         playerText = `${currentPlayer} has won!`
         console.log(playerText)
         alert(playerText)
+        return
     }
-  
+    currentPlayer = currentPlayer == x_test ? o_test : x_test
+    updatePlayerName()
 }
 
-restart.addEventListener('click', restartBtn)
-
-
-function restartBtn(){
+restartBtn.addEventListener('click',restart)
+function restart(){
     spaces.fill(null)
 
     board.forEach( board => {
         board.innerText = ''
-        currentPlayer = x_test
     })
+    currentPlayer = x_test
 }
+
 function playerHasWon() {
     for (const condition of winningCombo) {
-        let [a, b, c] = condition  
+        let [a, b, c] = condition;
 
-        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])){
-            return [a,b,c]
+        if (spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c]) {
+            return currentPlayer === x_test ? player1Name : player2Name;
         }
-
     }
-    return false
+
+    return false;
 }
 
+function updatePlayerName() {
+    if (player1 && player2) {
+        if (currentPlayer == x_test) {
+            playerName.innerText = player1
+            playerName2.innerText = ''
+        } else {
+            playerName.innerText = ''
+            playerName2.innerText = player2
+        }
+    }
+}
 
 playerName1Btn.addEventListener('click', function(evt){
-    evt.preventDefault ()
-})
+    evt.preventDefault();
+    player1Name = document.getElementById('player-title').value;
+    playerName.innerText = `${player1Name}: ${x_test}`;
+});
 
 playerName2Btn.addEventListener('click', function(evt){
-    evt.preventDefault ()
-})
+    evt.preventDefault();
+    player2Name = document.getElementById('player-title2').value;
+    playerName2.innerText = `${player2Name}: ${o_test}`;
+});
 startGame()
